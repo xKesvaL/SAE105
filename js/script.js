5b8eec3f13b84cb84085cf9eaa32d77d51ceb9a6
unknown <xKesvaL>
2022-11-10 21:06:44 +0100

    Theme-Switcher done, responsive and JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
+const themes = document.querySelectorAll(`[name="theme"]`);
+
 window.addEventListener("load", () => {
-  document.querySelector("body").classList.remove("preload");
+  themes.forEach((theme) => {
+    theme.addEventListener("click", () => {
+      storeTheme(theme.id);
+    });
+  });
+
+  applyTheme();
   createBurgerMenu();
   listenToBurger();
+
+  // Always last : activates all transitions.
+  document.querySelector("body").classList.remove("preload");
 });
+
+const storeTheme = (theme) => {
+  localStorage.setItem("theme", theme);
+};
+
+const applyTheme = (theme) => {
+  const activeTheme = localStorage.getItem("theme");
+  themes.forEach((theme) => {
+    if (theme.id == activeTheme) {
+      theme.checked = true;
+    }
+  });
+};
 
 const createBurgerMenu = () => {
   let pages = document.querySelector("header .pages");
@@ -15,12 +39,19 @@ const listenToBurger = () => {
   let burger = document.querySelector("header .burger");
   let burgerMenu = document.querySelector(".burger-menu");
   let burgerSvg = document.querySelector("header .burger svg");
+  let logo = document.querySelector("header a:nth-child(2)");
+  let burgerAnchors = document.querySelectorAll(".burger-menu a");
+  let placeHolder = document.querySelector(".header-placeholder");
+  burgerAnchors.forEach((anchor) => {
+    anchor.addEventListener("click", () => {
+      burger.click();
+    });
+  });
+
   burger.addEventListener("click", () => {
+    placeHolder.classList.toggle("active");
     burgerMenu.classList.toggle("burger-shown");
     burgerSvg.classList.toggle("white-svg");
-    let burgerAnchors = document.querySelectorAll("header a");
-    burgerAnchors.forEach((anchor) => {
-      anchor.classList.toggle("burger-header");
-    });
+    logo.classList.toggle("burger-header");
   });
 };
